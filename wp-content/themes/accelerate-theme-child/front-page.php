e6fda664a6f0bb8690985c7b0078b419f39d899f
ahaefele <ahaefele@greenmail.net>
2018-05-18 11:58:43 -0700

    changes to social media menu and custom archive

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -17,10 +17,31 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
+<section class="featured-work">
+	<div class="site-content">
+		<h2>Featured Work</h2>
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		<?php while ( have_posts() ) : the_post();
+			$image_1 = get_field("image_1");
+			$size = "medium";
+		?>
+			<li class="individual-featured-work">
+				<figure>
+					<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image($image_1, $size); ?></a>
+				</figure>
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
+		 <?php endwhile; ?>
+		 <?php wp_reset_query(); ?>
+	 </ul>
+	</div>
+</section>
+
 	<section class="recent-posts">
  <div class="site-content">
 	 <div class="blog-post">
@@ -28,7 +49,8 @@ get_header(); ?>
 		 <?php query_posts('posts_per_page=1'); ?>
 		 <?php while ( have_posts() ) : the_post(); ?>
 			 <h3><?php the_title(); ?></h3>
-       <?php the_excerpt(); ?> 
+       <?php the_excerpt(); ?>
+			 <a href="<?php the_permalink(); ?>" class="read-more-link"></a>
   		<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
  </div>
